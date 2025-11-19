caaad5581153a8c39f43d64525fb8ed8bce3c388
Amiri <0xamiridev@gmail.com>
2025-11-19 16:04:28 +0430

    modifed about.py from mobile

--- src/pwgenx/about.py
+++ src/pwgenx/about.py
@@ -1,12 +1,12 @@
 ABOUT=\
 """
-================================= PASSWORD GENERATOR =================================\n
-=================================   version: 0.1     =================================
+=================================  PWGENX  =================================\n
+==================================  v0.1  ==================================
 
-By this tool you can generate password for password suggestion with custom characters.
-It uses secure algorithm to generate password and can be secure if length is considered;
-the longer length, the better, but make sure your hashing algorithm supports the 
-provided length which some might not support length 128 fully. 
+Generate secure, customizable passwords for password suggestions.
+It uses secure algorithms to generate password and can be secure if length is considered;
+the longer length, the better, but make sure your hashing algorithm supports the
+provided length which some might not support length 128 fully.
 """
 
 
